3f56b48074b1b73ffb32cd9ba6c20e902771799b
darkimport <darkimport@gmail.com>
2012-03-25 06:21:47 +0000

    Using OmegleSpyExpertMainWindow in App (temporarily).
    Fixed a bug in SPyController. Was not initializing conversationEnded correctly.
    
    Refined OmegleSpyExpertMainWindow.
    
    Added ShortcutKeyHelper and accompanying classes for parsing and translating keyboard shortcuts.
    
    Added keyboard shortcuts to DEFAULTSETTINGS.properties.
    
    Removed title pane from OmegleSpyExpertMainWindow.yml.

--- omeglespy-z/src/main/java/org/darkimport/omeglespy/App.java
+++ omeglespy-z/src/main/java/org/darkimport/omeglespy/App.java
@@ -10,7 +10,7 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.darkimport.configuration.ConfigHelper;
-import org.darkimport.omeglespy.ui.OmegleSpyMainWindow;
+import org.darkimport.omeglespy.ui.OmegleSpyExpertMainWindow;
 import org.javabuilders.swing.SwingJavaBuilder;
 
 /**
@@ -51,7 +51,8 @@ public class App {
 				SwingJavaBuilder.getConfig().addResourceBundle("LogViewerWindow");
 				try {
 					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-					new OmegleSpyMainWindow().setVisible(true);
+					// new OmegleSpyMainWindow().setVisible(true);
+					new OmegleSpyExpertMainWindow().setVisible(true);
 				} catch (final Exception e) {
 					e.printStackTrace();
 				}
