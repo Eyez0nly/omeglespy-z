e93f04685cde3de304be527247e8bd891eea5dc2
darkimport@gmail.com <darkimport@gmail.com@30e68092-877d-9097-e134-a658bd403605>
2012-04-11 13:36:45 +0000

    Initial import.

---
+++ trunk/config-helper/src/main/java/org/darkimport/config/ConfigState.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package org.darkimport.config;
+
+/**
+ * @author user
+ * 
+ */
+public enum ConfigState {
+	GOOD, CONTAINS_DEPRECATED, CONTAINS_INVALID, UPDATE_REQUIRED;
+}
---
+++ trunk/config-helper/src/main/java/org/darkimport/config/ConfigurationMetadata.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package org.darkimport.config;
+
+import java.util.List;
+
+/**
+ * @author user
+ * 
+ */
+public class ConfigurationMetadata {
+	private List<ConfigurationMetadataElement>	metadataElements;
+
+	public static ConfigurationMetadata loadConfigurationMetadata(final String metadataFilename) {
+		// TODO
+		return null;
+	}
+
+	/**
+	 * @return the metadataElements
+	 */
+	public List<ConfigurationMetadataElement> getMetadataElements() {
+		return metadataElements;
+	}
+}
