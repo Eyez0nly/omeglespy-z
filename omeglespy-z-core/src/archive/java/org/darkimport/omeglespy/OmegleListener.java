bd8aa4d2ca206ba88aea46236b2e029837a985ca
darkimport <darkimport@gmail.com>
2012-04-12 13:40:38 +0000

    Created 0.0.1-BETA-001 tag for cl

--- omeglespy-z-core/src/archive/java/org/darkimport/omeglespy/OmegleListener.java
+++
@@ -1,8 +0,0 @@
-package org.darkimport.omeglespy;
-
-@Deprecated
-interface OmegleListener {
-	public void eventFired(Omegle src, String event, String... args);
-
-	public void messageSent(Omegle src, String msg);
-}
