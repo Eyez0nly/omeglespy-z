291808ecd1091a3f3d6dac2cf22e5554b2d7f97b
darkimport <darkimport@gmail.com>
2012-04-15 23:43:31 +0000

    - Added a method to the controller that checks if the conversation is active.
    - Added a main class that informs a user that this is not an application library.

---
+++ omeglespy-z-core/src/main/java/No.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane;
+
+/**
+ * 
+ */
+
+/**
+ * @author user
+ * 
+ */
+public class No {
+
+	/**
+	 * @param args
+	 */
+	public static void main(final String[] args) {
+		JOptionPane
+				.showMessageDialog(
+						null,
+						"This library does nothing on its own. Visit http://code.google.com/p/omeglespy-z for more information.",
+						"NO.", JOptionPane.ERROR_MESSAGE);
+	}
+
+}
